d5642c97fd60cda381635545f99c8dcc6bb69e76
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2024-04-25 07:41:00 +0300

    [Task-3] feat: Task 3 compeleted

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,10 @@
+import { uploadPhoto, createUser } from "./utils.js";
+
+export default function handleProfileSignup() {
+   return Promise.all([uploadPhoto(), createUser()]).then((response) => {
+        console.log(`${response[0].body} ${response[1].firstName} ${response[1].lastNameዝዝ}`);
+    }).catch(() => {
+        console.log("Signup system offline")
+    });
+}
+handleProfileSignup()
