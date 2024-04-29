6df7c4782586f7bca0a14e5b0933930a6f78c8cb
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2024-04-29 10:22:28 +0300

    [Task-4] Task 4 completed

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -2,4 +2,5 @@ export default function getListStudentIdsSums(listStudents) {
   if (listStudents instanceof Array) {
     return listStudents.reduce((sum, student) => sum + student.id, 0);
   }
+  return [];
 }
