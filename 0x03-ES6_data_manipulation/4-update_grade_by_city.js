49be6a88575d296462a17f84908a2d44db5b0ef3
Bisrat Maru Shibeshi <bisratmaru111@gmail.com>
2024-05-01 09:21:54 +0300

    [Task-6] Modified: increased readability

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,11 +1,21 @@
-export default function updateStudentGradeByCity(listStudents, city, newGrades) {
-    if (listStudents instanceof Array) {
-        return listStudents.filter((student) => student.location === city).map((student) => ({
-            id: student.id,
-            firstName: student.firstName,
-            location: student.location,
-            grade:(newGrades.filter((grade) => grade.studentId === student.id).pop() || {grade: 'N/A'}).grade,
-        }));
-    }
-    return [];
+export default function updateStudentGradeByCity(
+  listStudents,
+  city,
+  newGrades
+) {
+  if (listStudents instanceof Array) {
+    return listStudents
+      .filter((student) => student.location === city)
+      .map((student) => ({
+        id: student.id,
+        firstName: student.firstName,
+        location: student.location,
+        grade: (
+          newGrades.filter((grade) => grade.studentId === student.id).pop() || {
+            grade: "N/A",
+          }
+        ).grade,
+      }));
+  }
+  return [];
 }
