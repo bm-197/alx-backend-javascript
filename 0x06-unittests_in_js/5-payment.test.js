f453536dcd8907a66c4fe96a3930fcd77426de25
bm-197 <bisratmaru111@gmail.com>
2024-07-25 21:49:54 +0300

    [Task 5] feat: Completed Task 5

--- 0x06-unittests_in_js/5-payment.test.js
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -17,15 +17,13 @@ describe("sendPaymentRequestToAPI", () => {
 
   it('sendPaymentRequestToApi(100, 20) logs "The total is: 120" to the console', () => {
     sendPaymentRequestToAPI(100, 20);
-    expect(spy.log.calledOnceWith("The total is: 120")).to.be.true;
+    expect(spy.log.calledWith("The total is: 120")).to.be.true;
     expect(spy.log.calledOnce).to.be.true;
-    spy.restore();
   });
 
   it('sendPaymentRequestToApi(10, 10) logs "The total is: 20" to the console', () => {
     sendPaymentRequestToAPI(10, 10);
     expect(spy.log.calledwith("The total is: 20")).to.be.true;
     expect(spy.log.calledOnce).to.be.true;
-    spy.log.restore();
   });
 });
