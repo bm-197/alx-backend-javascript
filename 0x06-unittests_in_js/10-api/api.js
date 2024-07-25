28ef0e0e23f94e76b7a1bb57e0154e7cece0142c
bm-197 <bisratmaru111@gmail.com>
2024-07-26 00:01:29 +0300

    [Task 10] feat: Completed Task 10

---
+++ 0x06-unittests_in_js/10-api/api.js
@@ -0,0 +1,40 @@
+const express = require("express");
+
+const app = express();
+const PORT = 7865;
+
+app.get("/", (_, res) => {
+  res.send("Welcome to the payment system");
+});
+
+app.get("/cart/:id(\\d+)", (req, res) => {
+  const id = req.params.id;
+
+  res.send(`Payment methods for cart ${id}`);
+});
+
+app.get("/available_payments", (_req, res) => {
+  res.send(
+    {
+      payment_methods: {
+        credit_cards: true,
+        paypal: false,
+      },
+    }
+  );
+});
+
+app.post('/login', (req, res) => {
+  let user_name;
+
+  if (body) {
+    user_name = req.body.userName;
+  }
+  res.send(`Welcome ${user_name}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`API available on localhost port ${PORT}`);
+});
+
+module.exports = app;
