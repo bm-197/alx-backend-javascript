d91455b8ae8bb1ecdf4eaed8723d4b1e8f619d54
bm-197 <bisratmaru111@gmail.com>
2024-07-25 23:47:32 +0300

    [Task 8] update: Modify Task 8 and 9

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,14 +1,13 @@
-const { expect } = require("chai");
-const app = require("./api");
 const request = require('request');
+const { expect } = require('chai');
 
-describe("Index page", () => {
-  const API_URL = "http://localhost:7865";
+describe('API integration test', () => {
+  const API_URL = 'http://localhost:7865';
 
-  it("GET / returns correct response", (done) => {
-    request.get(`${API_URL}/`, (_err, res) => {
+  it('GET / returns correct response', (done) => {
+    request.get(`${API_URL}/`, (_err, res, body) => {
       expect(res.statusCode).to.be.equal(200);
-      expect(res.body).to.be.equal("Welcome to the payment system");
+      expect(body).to.be.equal('Welcome to the payment system');
       done();
     });
   });
