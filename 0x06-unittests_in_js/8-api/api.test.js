89448380289cbf04259542caac120a83dccad17e
bm-197 <bisratmaru111@gmail.com>
2024-07-25 23:16:09 +0300

    [Task skk 8] feat: Completed Task 8

---
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -0,0 +1,15 @@
+const { expect } = require("chai");
+const app = require("./api");
+const request = require('request');
+
+describe("Return the index page", () => {
+  const API_URL = "http://localhost:7865";
+
+  it("GET / returns correct response", (done) => {
+    request.get(`${API_URL}/`, (_err, res) => {
+      expect(res.statusCode).to.be.equal(200);
+      expect(res.body).to.be.equal("Welcome to the payment system");
+      done();
+    });
+  });
+});
