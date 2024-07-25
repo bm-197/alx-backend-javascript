14313d1536e6139d5a184816d97a03b6a59d2aa3
bm-197 <bisratmaru111@gmail.com>
2024-07-25 19:37:04 +0300

    [Task 3] feat: Completed Task 3

---
+++ 0x06-unittests_in_js/utils.js
@@ -0,0 +1,16 @@
+const Utils = {
+  calculateNumber(type, a, b) {
+    a = Math.round(a);
+    b = Math.round(b);
+
+    if (type == "SUM") {
+      return a + b;
+    } else if (type == "SUBTRACT") {
+      return a - b;
+    } else if (type == "DIVIDE") {
+      return b === 0 ? "Error" : a / b;
+    }
+  },
+};
+
+module.exports = Utils;
