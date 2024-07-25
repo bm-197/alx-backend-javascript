2cb171b887e48ef8563ccffc76e919f8c4e490bb
bm-197 <bisratmaru111@gmail.com>
2024-07-25 22:24:52 +0300

    [Task 7] feat: Completed Task 7

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,13 +1,11 @@
-const getPaymentTokenFromAPI = require('./6-payment_token');
-const expect = require('chai');
+const getPaymentTokenFromAPI = require("./6-payment_token");
+const expect = require("chai");
 
-
-describe('getPaymentTokenFromAPI', () => {
-    it('getPaymentTokenFromAPI(true)', () => {
-        getPaymentTokenFromAPI(true)
-        .then((res) => {
-            expect(res).to.deep.equal({data: 'Successful response from the API' });
-            done();
-        });
+describe("getPaymentTokenFromAPI", () => {
+  it("getPaymentTokenFromAPI(true)", () => {
+    getPaymentTokenFromAPI(true).then((res) => {
+      expect(res).to.deep.equal({ data: "Successful response from the API" });
+      done();
     });
+  });
 });
